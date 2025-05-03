827eef694dcba833a4e4c214cace244831f7bfd1
Peipr <m.rocacugat@student.maastrichtuniversity.nl>
2025-05-03 20:39:48 +0200

    Hackery shit

--- Scripts/Gemini/Make cache/uploadCache.py
+++ Scripts/Gemini/Make cache/uploadCache.py
@@ -3,19 +3,19 @@ from google.genai import types
 import io
 import httpx
 
-client = genai.Client(api_key="AIzaSyCxi987e4WEYQALikV8KwF8usDFp8ur2VA")
+client = genai.Client(api_key="")
 
 long_context_pdf_path = "https://cdn.discordapp.com/attachments/852189042552078356/1368157384031080448/Cupra_manual_compressed.pdf?ex=6817335b&is=6815e1db&hm=1d4d80339663b6e49294f504ab5449271ca245b6d5375a01aa52d0b0b9cb33bb&"
 
 # Retrieve and upload the PDF using the File API
-doc_io = "Cupra_manual.txt"
+doc_io = "./Cupra_manual.pdf"
 
 document = client.files.upload(
   file=doc_io,
-  config=dict(mime_type='text/plain'),
+  config=dict(mime_type='application/pdf'),
 )
 
-model_name = "gemini-1.5-flash-8b"
+model_name = "gemini-1.5-flash"
 system_instruction = "You are a car support rep, and you must help users know how to use the Cupra Tavascan"
 
 # Create a cached content object
