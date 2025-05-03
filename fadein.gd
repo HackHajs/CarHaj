a11195539dbf8d047c4e815a40050c33e392d6f7
the1Rogue <fabiantschut@gmail.com>
2025-05-03 23:30:09 +0200

    fixed main menu buttons

--- fadein.gd
+++ fadein.gd
@@ -1,6 +1,7 @@
-extends TextureRect
+extends Control
 
 func _ready() -> void:
+	modulate.a = 0
 	await get_tree().create_timer(2).timeout
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate:a", 1, 1)
